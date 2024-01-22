ba0ec0ccbbf56773026382f38bb8b07c3401824f
dimitris <iracleous@gmail.com>
2024-01-22 10:18:32 +0200

    testing

--- TestEshop/UnitTestCustomerService.cs
+++ TestEshop/UnitTestCustomerService.cs
@@ -35,53 +35,59 @@ public class UnitTestCustomerService
     }
 
 
-    [Fact]
-    public async Task TestAddCustomer()
+    //[Fact]
+    //public async Task TestAddCustomer()
+    //{
+    //    var customer = new Customer { };
+
+    //    var customers = new List<Customer>
+    //    {
+    //        new() { Id = 1, Name = "D1" },
+    //        new() { Id = 2, Name = "D2" }
+    //    };
+
+    //    Assert.NotNull(customer);
+
+    //    var mockDbContext = new Mock<EshopDbContext>();
+    //    var mockLogger = new Mock<ILogger<CustomerServices>>();
+
+    //    mockDbContext.Setup(db => db.Customers.Find(It.IsAny<int>()))
+    //            .Returns<Customer>(customer => customers.SingleOrDefault(
+    //                searchCustomer => searchCustomer.Id == customer.Id));
+
+    //    ICustomerServices services = new CustomerServices(
+    //        mockDbContext.Object, mockLogger.Object);
+    //    Act
+    //   var result = await services.GetCustomersAsync();
+
+    //    Assert
+    //    Assert.NotNull(result);
+    //    Assert.Equal(0, result.Status);
+    //}
+
+    [Theory]
+    [InlineData(3, 2, 5)]  // (3 + 2) * (3 - 2) = 5
+    [InlineData(0, 5, -25)]  // (0 + 5 )* (0 - 5) = -25
+    [InlineData(10, 5, 75)]  // (10 + 5) * (10 - 5) = 75
+    [InlineData(0, 0, 0)]  // (0 + 0) * (0 - 0) = 0
+    [InlineData(-10, -50, -2400)]  // (-10 + -50) * (-10 - -50) = -2400
+    public void PerformComplexCalculation_ShouldReturnExpectedResult
+        (int a, int b, int expectedResult)
     {
-        var customer = new Customer { };
+        // Arrange
+        var mockCalculationService = new Mock<ICalculationService>();
 
-        var testData = new List<Customer>
-        {
-            new() { Id = 1, Name = "D1" },
-            new() { Id = 2, Name = "D2" }
-        };
+        mockCalculationService.Setup(service => service.Add(It.IsAny<int>(), It.IsAny<int>()))
+                              .Returns<int, int>((x, y) => x + y);
+        mockCalculationService.Setup(service => service.Subtract(It.IsAny<int>(), It.IsAny<int>()))
+                              .Returns<int, int>((x, y) => x - y);
 
-        Assert.NotNull(customer);
+        var calculator = new Calculator(mockCalculationService.Object);
 
-        var mockDbContext = new Mock<EshopDbContext>();
-        var mockLogger = new Mock<ILogger<CustomerServices>>();
-
-        mockDbContext.Setup(db => db.Customers.Find(It.IsAny<int>()))
-                .Returns<int>(id => testData.SingleOrDefault(p => p.Id == id));
-
-        ICustomerServices services = new CustomerServices(mockDbContext.Object, mockLogger.Object);
         // Act
-        var result = await services.GetCustomersAsync();
+        int result = calculator.PerformComplexCalculation(a, b);
 
         // Assert
-        Assert.NotNull(result);
-        Assert.Equal(0, result.Status);
+        Assert.Equal(expectedResult, result);
     }
-
-    //[Theory]
-    //[InlineData(3, 2, 5 )]  // 3 + 2 * (3 - 2) = 5
-    //[InlineData(0, 5, 0)]  // 0 + 5 * (0 - 5) = 0
-    //[InlineData(10, 5, -50)]  // 10 + 5 * (10 - 5) = -50
-    //public void PerformComplexCalculation_ShouldReturnExpectedResult(int a, int b, int expectedResult)
-    //{
-    //    // Arrange
-    //    var mockCalculationService = new Mock<ICalculationService>();
-    //    mockCalculationService.Setup(service => service.Add(It.IsAny<int>(), It.IsAny<int>()))
-    //                          .Returns<int, int>((x, y) => x + y);
-    //    mockCalculationService.Setup(service => service.Subtract(It.IsAny<int>(), It.IsAny<int>()))
-    //                          .Returns<int, int>((x, y) => x - y);
-
-    //    var calculator = new Calculator(mockCalculationService.Object);
-
-    //    // Act
-    //    int result = calculator.PerformComplexCalculation(a, b);
-
-    //    // Assert
-    //    Assert.Equal(expectedResult, result);
-    //}
 }
